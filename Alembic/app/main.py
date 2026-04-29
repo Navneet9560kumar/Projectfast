7455181f1ab7dc6868f3d5a54a4dd9b2abb99959
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2026-04-30 01:27:45 +0530

    Allow all CORS origins for testing

--- Alembic/app/main.py
+++ Alembic/app/main.py
@@ -47,7 +47,7 @@ app = FastAPI(
 #  CORS MIDDLEWARE 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "https://projectfast-q9v2.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
